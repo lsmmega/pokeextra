b01bc75bd34bcf2d72c2bfb2de1bc84b824c8080
lsmmega <pogmegalsm@outlook.com>
2024-10-05 17:15:33 +0800

    Revert "add running under tutorial"
    This reverts commit f7f84a623be8ad08c32cafc79e01787a4a71da4f.

--- data/sprites/player_sprites.asm
+++ data/sprites/player_sprites.asm
@@ -3,7 +3,6 @@ ChrisStateSprites:
 	db PLAYER_BIKE,      SPRITE_CHRIS_BIKE
 	db PLAYER_SURF,      SPRITE_SURF
 	db PLAYER_SURF_PIKA, SPRITE_SURFING_PIKACHU
-	db PLAYER_RUN,       SPRITE_CHRIS
 	db -1 ; end
 
 KrisStateSprites:
@@ -11,5 +10,4 @@ KrisStateSprites:
 	db PLAYER_BIKE,      SPRITE_KRIS_BIKE
 	db PLAYER_SURF,      SPRITE_SURF
 	db PLAYER_SURF_PIKA, SPRITE_SURFING_PIKACHU
-	db PLAYER_RUN,       SPRITE_KRIS
 	db -1 ; end
